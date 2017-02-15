21fde37dde7e57599df740b3b489d7047060a9c7
Johannes Lauinger <johannes@lauinger-it.de>
2017-02-15 17:32:00 +0100

    add dummmy tests

---
+++ tests/DummyTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+
+final class DummyTest extends TestCase {
+
+    public function testNothing(): void {
+        $this->assertEquals(true, true);
+    }
+}
+
