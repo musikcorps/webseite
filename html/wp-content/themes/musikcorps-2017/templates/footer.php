06310d5a7e28e00843658209b331f9d90e35ebaf
Johannes Lauinger <johannes@lauinger-it.de>
2017-04-24 02:37:27 +0200

    some corrections

--- html/wp-content/themes/musikcorps-2017/templates/footer.php
+++ html/wp-content/themes/musikcorps-2017/templates/footer.php
@@ -1,12 +1,14 @@
-<footer>
+<footer class="page-footer">
     <?php dynamic_sidebar('sidebar-footer'); ?>
     <div class="text-center">
         <div class="site-info">
             &copy; <?= date('Y') ?> Musikcorps Niedernberg e.V.
             <span class="sep"> | </span>
-            Proudly powered by <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'musikcorps-2017' ) ); ?>">Wordpress</a>
+            <a href="#">Impressum</a>
             <span class="sep"> | </span>
-            <?php printf( esc_html__( 'Theme: %1$s von %2$s.', 'musikcorps-2017' ), 'Musikcorps 2017', '<a href="https://johannes-lauinger.de/" rel="designer">Johannes Lauinger</a>' ); ?>
+            Proudly powered by <a href="https://wordpress.org/" target="_blank">Wordpress</a>
+            <span class="sep"> | </span>
+            Theme by <a href="https://johannes-lauinger.de/" rel="designer" target="_blank">Johannes Lauinger</a>
         </div>
     </div>
 </footer>
