f89ed57bf28d7c0a9ae75ae62b6e838e156b99eb
Johannes Lauinger <johannes@lauinger-it.de>
2017-04-30 22:48:57 +0200

    fix issue with undefined item for new item page

--- html/wp-content/mu-plugins/musikcorps-members/templates/admin_edit.html.php
+++ html/wp-content/mu-plugins/musikcorps-members/templates/admin_edit.html.php
@@ -8,36 +8,34 @@
         <table class='wp-list-table fixed'>
             <tr>
                 <th class="ss-th-width"><label for="firstname"><?= __('Vorname') ?></label></th>
-                <td><input type="text" name="firstname" id="firstname" value="<?= $this->item->firstname ?>" class="ss-field-width" /></td>
+                <td><input type="text" name="firstname" id="firstname" value="<?= isset($this->item) ? $this->item->firstname : '' ?>" class="ss-field-width" /></td>
             </tr>
             <tr>
                 <th class="ss-th-width"><label for="lastname"><?= __('Nachname') ?></label></th>
-                <td><input type="text" name="lastname" id="lastname" value="<?= $this->item->lastname ?>" class="ss-field-width" /></td>
+                <td><input type="text" name="lastname" id="lastname" value="<?= isset($this->item) ? $this->item->lastname : '' ?>" class="ss-field-width" /></td>
             </tr>
             <tr>
                 <th class="ss-th-width"><label for="instrument"><?= __('Instrument') ?></label></th>
-                <td><input type="text" name="instrument" id="instrument" value="<?= $this->item->instrument ?>" class="ss-field-width" /></td>
+                <td><input type="text" name="instrument" id="instrument" value="<?= isset($this->item) ? $this->item->instrument : '' ?>" class="ss-field-width" /></td>
             </tr>
             <tr>
                 <th class="ss-th-width"><label for="register"><?= __('Register', 'musikcorps') ?></label></th>
-                <td><input type="text" name="register" id="register" value="<?= $this->item->register ?>" class="ss-field-width" /></td>
+                <td><input type="text" name="register" id="register" value="<?= isset($this->item) ? $this->item->register : '' ?>" class="ss-field-width" /></td>
             </tr>
             <tr>
                 <th class="ss-th-width"><label for="birthday"><?= __('Geburtstag') ?></label></th>
-                <td><input type="text" name="birthday" id="birthday" value="<?= $this->item->birthday ?>" class="ss-field-width" placeholder="YYYY-MM-DD" /></td>
+                <td><input type="text" name="birthday" id="birthday" value="<?= isset($this->item) ? $this->item->birthday : '' ?>" class="ss-field-width" placeholder="YYYY-MM-DD" /></td>
             </tr>
             <tr>
                 <th class="ss-th-width"><label for="email"><?= __('E-Mail') ?></label></th>
-                <td><input type="text" name="email" id="email" value="<?= $this->item->email ?>" class="ss-field-width" /></td>
+                <td><input type="text" name="email" id="email" value="<?= isset($this->item) ? $this->item->email : '' ?>" class="ss-field-width" /></td>
             </tr>
         </table>
         <p></p>
         <?php if(isset($_GET["id"])) { echo '<input type="hidden" name="id" value="'.$_GET["id"].'" />'; } ?>
         <input type="hidden" name="action" value="musikcorps_save_member" />
         <input type="submit" name="musikcorps_save_member" value="<?= __('Speichern') ?>" class="button button-primary button-large" />
-        <?php if(isset($_GET["id"])) { echo '<input type="submit" name="musikcorps_delete_member" value="Löschen" class="button button-large" />'; } ?>
-    </form>
-</div>
+        <?php if(isset($_GET["id"])) { echo '<input type="submit" name="musikcorps_delete_member" value="Löschen" class="button button-large" />'; } ?></form></div>
 
 
 <style>
