8065d7d3530b8f84ab082917942cf26c1d7be8ff
Johannes Lauinger <johannes@lauinger-it.de>
2017-04-30 20:20:38 +0200

    remove footer imprint link

--- html/wp-content/themes/musikcorps-2017/templates/footer.php
+++ html/wp-content/themes/musikcorps-2017/templates/footer.php
@@ -4,8 +4,6 @@
         <div class="site-info">
             &copy; <?= date('Y') ?> Musikcorps Niedernberg e.V.
             <span class="sep"> | </span>
-            <a href="#">Impressum</a>
-            <span class="sep"> | </span>
             Proudly powered by <a href="https://wordpress.org/" target="_blank">Wordpress</a>
             <span class="sep"> | </span>
             Theme by <a href="https://johannes-lauinger.de/" rel="designer" target="_blank">Johannes Lauinger</a>
