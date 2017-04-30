73b27bb1bc8cff4d92b17ff1cd33b1ae1a1b48cb
Johannes Lauinger <johannes@lauinger-it.de>
2017-05-01 00:24:25 +0200

    gallery features and design

--- html/wp-content/themes/musikcorps-2017/assets/scripts/main.js
+++ html/wp-content/themes/musikcorps-2017/assets/scripts/main.js
@@ -2,6 +2,27 @@
 
     "use strict";
 
+    // kick off Foundation
     $(document).foundation();
+
+
+    $(function() {
+        // only fire on gallery slide page
+        if($('.lg_gallery.slide').length === 0) return;
+        // attach key listener to switch slides
+        $(document).keydown(function(e) {
+            switch (e.which) {
+                case 37: // left arrow
+                    location.href = $('.prev-page').attr('href');
+                    break;
+                case 39: // right arrow
+                    location.href = $('.next-page').attr('href');
+                    break;
+                default:
+                    return;
+            }
+            e.preventDefault();
+        });
+    });
 
 })( window, document, jQuery );
